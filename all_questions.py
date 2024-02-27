65b830cb491526cd90e8db5aed20db437fb374aa
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-02-27 18:29:29 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -104,7 +104,7 @@ def question3():
     answers = {}
 
     # type: a string that evaluates to a float
-    answers["(a) SSE"] = 7.5
+    answers["(a) SSE"] = "4 * R ** 2"
 
     # type: a string that evaluates to a float
     answers["(b) SSE"] = 7.5
